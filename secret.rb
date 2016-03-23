691a28ff9985f584ea9372caa2b2fc1ee0be8122
Kerry <kerry.sheldon@gmail.com>
2016-03-23 13:28:44 -0600

    variable game size array and some new secret functions

--- secret.rb
+++ secret.rb
@@ -2,33 +2,45 @@
 
 class Secret
 
-  def initialize(num_elements)
-    @num_elements = num_elements
-    @colors = ['r', 'g', 'b', 'y']
+  def initialize(game_size)
+    @game_size = game_size
+    @colors = ['r', 'g', 'b', 'y', 'p', 'o']
     @secret = []
   end
 
-  attr_reader :num_elements
+  attr_reader :game_size
   attr_reader :colors
   attr_reader :secret
 
   def generate_random_color
-    color_position = rand(@colors.length)
+    color_position = rand(@game_size)
     color = @colors[color_position]
   end
 
   def generate_secret
-    num_elements.times do
+    game_size.times do
       @secret.push(generate_random_color)
     end
-    p @secret
+  end
+
+  def how_many(color)
+    @secret.count(color)
+  end
+
+  def color_at_position(index)
+    @secret[index]
   end
 
 end
 
 if __FILE__ == $0
-  test = Secret.new(30)
-  puts test.num_elements
+  test = Secret.new(6)
+  p test.game_size
   test.generate_secret
+  p test.secret
+  p test.how_many('o')
+  p test.how_many('r')
+  p test.color_at_position(0)
+  p test.color_at_position(4)
 
 end
